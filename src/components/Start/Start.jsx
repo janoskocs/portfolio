c5e9a47d2508e99d65018ae5a5fbc4c87b27e11a
Janos Kocs <janos.kocs@outlook.com>
2023-10-05 18:02:46 +0100

    feature: implement start menu buttons

--- src/components/Start/Start.jsx
+++ src/components/Start/Start.jsx
@@ -5,16 +5,46 @@ const Start = ({ showStartMenu }) => {
   return (
     <nav className={showStartMenu ? "start" : "start start--hidden"}>
       <section className="start__title-wrapper">
-        <img src={logo} alt="Microchip" className="start__image" />
+        <img src={logo} alt="Microchip" className="start__logo" />
         <h1 className="start__title">János Kócs | Portfolio</h1>
       </section>
       <ul className="start__list">
-        <li className="start__list-item">Menu 1</li>
-        <li className="start__list-item">Menu 2</li>
-        <li className="start__list-item">Menu 3</li>
-        <li className="start__list-item">Menu 1</li>
-        <li className="start__list-item">Menu 2</li>
-        <li className="start__list-item">Menu 3</li>
+        <li className="start__list-item">
+          <button className="start__button">
+            <img src={logo} alt="About me" className="start__menu-icon" />
+            Placeholder
+          </button>
+        </li>
+        <li className="start__list-item">
+          <button className="start__button">
+            <img src={logo} alt="GitHub" className="start__menu-icon" />
+            GitHub Page
+          </button>
+        </li>
+        <li className="start__list-item">
+          <button className="start__button">
+            <img src={logo} alt="LinkedIn" className="start__menu-icon" />
+            LinkedIn Profile
+          </button>
+        </li>
+        <li className="start__list-item">
+          <button className="start__button">
+            <img src={logo} alt="My projects" className="start__menu-icon" />
+            My Projects
+          </button>
+        </li>
+        <li className="start__list-item">
+          <button className="start__button">
+            <img src={logo} alt="Contact me" className="start__menu-icon" />
+            Contact Me
+          </button>
+        </li>
+        <li className="start__list-item">
+          <button className="start__button">
+            <img src={logo} alt="About me" className="start__menu-icon" />
+            About Me
+          </button>
+        </li>
       </ul>
     </nav>
   );
