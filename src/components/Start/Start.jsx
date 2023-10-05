a40b683ab9d747b61e003072db974936a576ce44
Janos Kocs <janos.kocs@outlook.com>
2023-10-05 16:58:04 +0100

    style: style startmenu sidebar

--- src/components/Start/Start.jsx
+++ src/components/Start/Start.jsx
@@ -1,9 +1,17 @@
 import "./Start.scss";
+import logo from "../../../public/images/skills/logo.png";
 
 const Start = ({ showStartMenu }) => {
   return (
-    <nav className={showStartMenu ? "start" : "start--hidden"}>
+    <nav className={showStartMenu ? "start" : "start start--hidden"}>
+      <section className="start__title-wrapper">
+        <img src={logo} alt="Microchip" className="start__image" />
+        <h1 className="start__title">János Kócs | Portfolio</h1>
+      </section>
       <ul className="start__list">
+        <li className="start__list-item">Menu 1</li>
+        <li className="start__list-item">Menu 2</li>
+        <li className="start__list-item">Menu 3</li>
         <li className="start__list-item">Menu 1</li>
         <li className="start__list-item">Menu 2</li>
         <li className="start__list-item">Menu 3</li>
