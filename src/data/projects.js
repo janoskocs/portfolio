bfa423f91736b188b134cd13789e58b402a9402c
Janos Kocs <janos.kocs@outlook.com>
2023-10-08 21:49:06 +0100

    feature: create first project object

---
+++ src/data/projects.js
@@ -0,0 +1,17 @@
+export const menuItems = [
+  {
+    id: "p1",
+    name: "MemoVault",
+    alt: "MemoVault",
+    icon: "/images/icons/mycomp.png",
+    action: "about-me",
+    type: "project",
+    address: "https://memovault.herokuapp.com",
+    date: "14/04/2023",
+    techstack: ["JavaScript", "React", "Express", "Node", "MongoDB"],
+    libraries: ["recharts", "mongoose"],
+    description: "lorem ipsum",
+    screenshots: [""],
+    github: "",
+  },
+];
