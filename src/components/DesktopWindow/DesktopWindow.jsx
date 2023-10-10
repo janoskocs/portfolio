b0c82f757ed22c9eb65159395de9c79e35600870
Janos Kocs <janos.kocs@outlook.com>
2023-10-10 12:12:28 +0100

    style: style basic window body and title

--- src/components/DesktopWindow/DesktopWindow.jsx
+++ src/components/DesktopWindow/DesktopWindow.jsx
@@ -1,6 +1,11 @@
 import "./DesktopWindow.scss";
 
 const DesktopWindow = ({ type }) => {
-  return <div className="window">{type}</div>;
+  return (
+    <div className="window">
+      <div className="window__title">{type}</div>
+      <div className="window__body">body</div>
+    </div>
+  );
 };
 export default DesktopWindow;
