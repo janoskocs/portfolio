2aff606cca03701096a71b04507236800698c86c
Janos Kocs <janos.kocs@outlook.com>
2023-10-10 12:23:30 +0100

    style: style icon and title wrappre in title bar

--- src/components/DesktopWindow/DesktopWindow.jsx
+++ src/components/DesktopWindow/DesktopWindow.jsx
@@ -3,7 +3,16 @@ import "./DesktopWindow.scss";
 const DesktopWindow = ({ type }) => {
   return (
     <div className="window">
-      <div className="window__title">{type}</div>
+      <div className="window__title-bar">
+        <div className="window__title-img-wrapper">
+          <img
+            src={`public/images/icons/contactme.png`}
+            alt=""
+            className="window__icon"
+          />
+          <h2 className="window__title">{type}</h2>
+        </div>
+      </div>
       <div className="window__body">body</div>
     </div>
   );
