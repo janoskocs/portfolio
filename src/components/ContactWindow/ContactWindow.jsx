03e14bd841f005308b4ab561abcc1ea535d43c07
Janos Kocs <janos.kocs@outlook.com>
2023-10-17 11:39:04 +0100

    feature: implement controlled component

--- src/components/ContactWindow/ContactWindow.jsx
+++ src/components/ContactWindow/ContactWindow.jsx
@@ -1,7 +1,20 @@
+import { useState } from "react";
 import DividerLine from "../DividerLine";
 import "./ContactWindow.scss";
 
 const ContactWindow = ({ handleOpenWindow, handleCloseWindow }) => {
+  const [input, setInput] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleInputChange = (e) => {
+    const form = e.target;
+    setInput({ ...input, [form.name]: form.value });
+  };
+
+  console.log(input);
   return (
     <div className="window">
       <div className="window__title-bar">
@@ -41,7 +54,11 @@ const ContactWindow = ({ handleOpenWindow, handleCloseWindow }) => {
             </a>
           </p>
           <DividerLine orientation="horizontal" />
-          <form action="" className="form">
+          <form
+            action="https://formsubmit.co/janos.kocs@outlook.com"
+            method="POST"
+            className="form"
+          >
             <fieldset className="form__fieldset">
               <legend className="form__legend">Send me a message</legend>
               <label htmlFor="name" className="form__label">
@@ -53,6 +70,8 @@ const ContactWindow = ({ handleOpenWindow, handleCloseWindow }) => {
                 id="name"
                 className="form__name-input"
                 placeholder="e.g. John Smith"
+                value={input.name}
+                onChange={(e) => handleInputChange(e)}
                 required
               />
               <label htmlFor="email" className="form__label">
@@ -64,6 +83,8 @@ const ContactWindow = ({ handleOpenWindow, handleCloseWindow }) => {
                 id="email"
                 className="form__email-input"
                 placeholder="e.g. john@outlook.com"
+                value={input.email}
+                onChange={(e) => handleInputChange(e)}
                 required
               />
 
@@ -71,8 +92,10 @@ const ContactWindow = ({ handleOpenWindow, handleCloseWindow }) => {
                 name="message"
                 id="message"
                 className="form__message-input"
-                required
+                value={input.message}
+                onChange={(e) => handleInputChange(e)}
                 placeholder="Hi Janos, let's work together..."
+                required
               ></textarea>
               <button className="form__send" type="submit">
                 Send
