662e3da603d8e958df1161d260125cadc39dae2a
Janos Kocs <janos.kocs@outlook.com>
2024-09-02 20:21:22 +0100

    update: remove ocado endpoint@

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/ocado" element={<Ocado />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
