5abae4b9e34ba74ea872454971b3ec9e42e2bf13
Janos Kocs <janos.kocs@outlook.com>
2024-09-07 15:55:12 +0100

    feat: install prettier and eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 const App = () => {
   return (
-      <h1>Software Portfolio</h1>
+    <h1>
+      <a href="https://www.google.com" target="_blank" rel="noreferrer">
+        link
+      </a>
+      Software Portfolio
+    </h1>
   );
 };
 
