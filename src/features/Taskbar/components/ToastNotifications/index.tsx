1215e1380803754a7c76a65b54a5e54cea26de71
Janos Kocs <janos.kocs@outlook.com>
2024-09-10 22:16:29 +0100

    feat: implement notification component

--- src/features/Taskbar/components/ToastNotifications/index.tsx
+++ src/features/Taskbar/components/ToastNotifications/index.tsx
@@ -3,7 +3,15 @@ import styles from "./ToastNotifications.module.css";
 const ToastNotifications = () => {
   return (
     <div className={styles["container"]}>
-      <div className={styles["notification"]}> TEST</div>
+      <div className={styles["notification"]}>
+        <button>x</button>
+        <h4>Title</h4>
+        <p>Content</p>
+        <div className={styles["notification-actions"]}>
+          <button>Accept</button>
+          <button>Decline</button>
+        </div>
+      </div>
       <img src="/images/icons/about-me.png" alt="Notifications" className={styles.icon} />
     </div>
   );
