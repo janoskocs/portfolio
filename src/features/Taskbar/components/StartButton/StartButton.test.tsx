3581e06e0efbba6925e1947e4221d2e6e17986c9
Janos Kocs <janos.kocs@outlook.com>
2024-09-15 18:46:30 +0100

    test: start button tests

--- src/features/Taskbar/components/StartButton/StartButton.test.tsx
+++ src/features/Taskbar/components/StartButton/StartButton.test.tsx
@@ -1,11 +1,17 @@
-import { render, screen } from "@testing-library/react";
-import StartButton from "./index";
+import { fireEvent, render, screen } from "@testing-library/react";
+import StartButton from ".";
 
 describe("StartButton Component", () => {
   it("should render the StartButton", () => {
-    render(<StartButton />);
+    const mockClick = vi.fn();
+
+    render(<StartButton onClick={mockClick} />);
     const buttonElement = screen.getByRole("button");
+    const icon = screen.getByRole("img");
     expect(buttonElement).toBeInTheDocument();
     expect(buttonElement).toHaveTextContent("Start");
+    expect(icon).toHaveAttribute("src", "/images/logos/logocolour.png");
+    fireEvent.click(buttonElement);
+    expect(mockClick).toHaveBeenCalledTimes(1);
   });
 });
